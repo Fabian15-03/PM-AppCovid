f6d9ba47eb4ef81ba5dc42aa13239338aba1360e
Alvaro Laupa <ronaldho_1910@hotmail.com>
2022-06-21 18:38:22 -0500

    fragment

--- app/src/main/java/pe/edu/ulima/pm/appcovid/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/appcovid/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import pe.edu.ulima.pm.appcovid.R.*
+import pe.edu.ulima.pm.appcovid.fragments.FragmentData
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentData = FragmentData()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
     }
     private fun mostrarFragmentSobreNosotros(ft: FragmentTransaction) {
-        ft.replace(id.secciones,fragmentSobreNosotros)
+        ft.replace(id.secciones,fragmentData)
     }
 }
