9449bca854b6c560c771f8d030e7669c72dcc417
Alvaro Laupa <ronaldho_1910@hotmail.com>
2022-06-21 18:54:39 -0500

    boton ver data

--- app/src/main/java/pe/edu/ulima/pm/appcovid/MenuActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/appcovid/MenuActivity.kt
@@ -1,24 +1,24 @@
 package pe.edu.ulima.pm.appcovid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import androidx.fragment.app.FragmentTransaction
-import pe.edu.ulima.pm.appcovid.R.*
+import androidx.fragment.app.FragmentActivity
 import pe.edu.ulima.pm.appcovid.fragments.FragmentData
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : FragmentActivity()  {
     private val fragmentData = FragmentData()
     private var mbtData: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-    }
-    private fun mostrarFragmentSobreNosotros(ft: FragmentTransaction) {
-        ft.replace(id.secciones,fragmentData)
+        mbtData=findViewById(R.id.btnData)
+        mbtData!!.setOnClickListener{v: View ->
+            //Camiando de view
+            val intent: Intent = Intent()
+            intent.setClass(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
