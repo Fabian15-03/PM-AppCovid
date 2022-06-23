ffceee26556c5097ac0392b53ddbd8b708448caf
Fabián Gonzales <luminition@hotmail.com>
2022-06-22 23:41:30 -0500

    Seat : Update

--- app/build/tmp/kapt3/stubs/debug/pe/edu/ulima/pm/appcovid/MainActivity.java
+++ app/build/tmp/kapt3/stubs/debug/pe/edu/ulima/pm/appcovid/MainActivity.java
@@ -2,8 +2,9 @@ package pe.edu.ulima.pm.appcovid;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u0014\u00a8\u0006\u0007"}, d2 = {"Lpe/edu/ulima/pm/appcovid/MainActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
-public final class MainActivity extends androidx.appcompat.app.AppCompatActivity {
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\bH\u0002J\u0012\u0010\t\u001a\u00020\u00062\b\u0010\n\u001a\u0004\u0018\u00010\u000bH\u0014R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\f"}, d2 = {"Lpe/edu/ulima/pm/appcovid/MainActivity;", "Landroidx/fragment/app/FragmentActivity;", "()V", "fragmentData", "Lpe/edu/ulima/pm/appcovid/fragments/FragmentData;", "mostrarFragmentSobreNosotros", "", "ft", "Landroidx/fragment/app/FragmentTransaction;", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class MainActivity extends androidx.fragment.app.FragmentActivity {
+    private final pe.edu.ulima.pm.appcovid.fragments.FragmentData fragmentData = null;
     
     public MainActivity() {
         super();
@@ -12,5 +13,8 @@ public final class MainActivity extends androidx.appcompat.app.AppCompatActivity
     @java.lang.Override()
     protected void onCreate(@org.jetbrains.annotations.Nullable()
     android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void mostrarFragmentSobreNosotros(androidx.fragment.app.FragmentTransaction ft) {
     }
 }
--- app/src/main/java/pe/edu/ulima/pm/appcovid/MenuActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/appcovid/MenuActivity.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import pe.edu.ulima.pm.appcovid.fragments.FragmentData
 
-class MenuActivity : FragmentActivity()  {
+class MenuActivity : AppCompatActivity()  {
     private var mbtData: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
